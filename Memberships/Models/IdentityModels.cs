bcb725467b3a691ae32c714f9d2354855596af9b
Dan <Dan@DESKTOP-ZALMAN>
2019-02-17 22:19:41 +0200

    Entity framework added tables

--- Memberships/Models/IdentityModels.cs
+++ Memberships/Models/IdentityModels.cs
@@ -1,4 +1,5 @@
-﻿using System.Data.Entity;
+﻿using System;
+using System.Data.Entity;
 using System.Security.AccessControl;
 using System.Security.Claims;
 using System.Threading.Tasks;
@@ -11,6 +12,10 @@ namespace Memberships.Models
     // You can add profile data for the user by adding more properties to your ApplicationUser class, please visit http://go.microsoft.com/fwlink/?LinkID=317594 to learn more.
     public class ApplicationUser : IdentityUser
     {
+        public string FirstName { get; set; }
+        public bool IsActive { get; set; }
+        public DateTime Registered { get; set; }
+
         public async Task<ClaimsIdentity> GenerateUserIdentityAsync(UserManager<ApplicationUser> manager)
         {
             // Note the authenticationType must match the one defined in CookieAuthenticationOptions.AuthenticationType
@@ -41,5 +46,18 @@ namespace Memberships.Models
         public DbSet<Item> Items { get; set; }
 
         public DbSet<Product> Products { get; set; }
+
+        public DbSet<ProductType> ProductTypes { get; set; }
+
+        public DbSet<ProductLinkText> ProductLinkTexts { get; set; }
+
+        public DbSet<Subscription> Subscriptions { get; set; }
+
+        public DbSet<ProductItem> ProductItems { get; set; }
+
+        public DbSet<SubscriptionProduct> SubscriptionProducts { get; set; }
+
+        public DbSet<UserSubscription> UserSubscriptions { get; set; }
+
     }
 }
